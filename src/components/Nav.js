fd03b92b33aade28c13693f372a07b6ae493e43b
maribelcuales <maribelcuales@gmail.com>
2022-09-04 08:14:57 +0800

    Added css styling to icons.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,6 @@
 import logo from '../images/crm-logo.png'; 
+import homeButton from '../images/home-button.png';
+import addButton from '../images/add-ticket-button.png'; 
 import { useNavigate } from 'react-router-dom';
 
 const Nav = () => {
@@ -6,13 +8,17 @@ const Nav = () => {
 	const navigate = useNavigate();
 
 	return (
-		<nav>
+		<nav className="nav-side-bar">
 			<div className="logo=container">
 				<img className="crm-logo" src={logo} alt="logo" />
 			</div>
-			<div className="conttrols-container">
-				<div className="icon" onClick={() => navigate('/ticket')}>Add</div>
-				<div className="icon" onClick={() => navigate('/')}>Home</div>
+			<div className="controls-container">
+				<div className="icon" onClick={() => navigate('/ticket')}>
+					<img className="add-icon" src={addButton} alt="add icon" />
+				</div>
+				<div className="icon" onClick={() => navigate('/')}>
+					<img className="home-icon" src={homeButton} alt="home icon" />
+				</div>
 			</div>
 		</nav>
 	)
