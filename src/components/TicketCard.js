fa05c81f66a674a616a60d285b3df414cfd66ced
maribelcuales <maribelcuales@gmail.com>
2022-09-06 18:27:05 +0800

    Added Link and components to TicketCard.

--- src/components/TicketCard.js
+++ src/components/TicketCard.js
@@ -1,7 +1,23 @@
+import { Link } from 'react-router-dom';
+import AvatarDisplay from './AvatarDisplay.js';
+import StatusDisplay from './StatusDisplay.js';
+import PriorityDisplay from './PriorityDisplay.js';
+import ProgessDisplay from './ProgressDisplay.js';
+import DeleteBlock from './DeleteBlock.js';
+
+
 const TicketCard = () => {
   return (
-    <div>
-      TicketCard
+    <div className="ticket-card">
+      <Link>
+        <div className='ticket-color'></div>
+        <h3>{title}</h3>
+        <AvatarDisplay />
+        <StatusDisplay />
+        <PriorityDisplay />
+        <ProgessDisplay />
+      </Link>
+      <DeleteBlock />
     </div>
   )
 
